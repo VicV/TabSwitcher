b7200f233325b78e957078d8b5145c0169a460bc
Sola Ogunsakin <oogunsakin@mozilla.com>
2014-02-24 01:53:11 -0800

    basic drag gestures in picker

---
+++ src/com/example/tabswitcher/Tab.java
@@ -0,0 +1,5 @@
+package com.example.tabswitcher;
+
+public class Tab {
+
+}
--- src/com/example/tabswitcher/TabSwitcher.java
+++ src/com/example/tabswitcher/TabSwitcher.java
@@ -1,13 +1,113 @@
 package com.example.tabswitcher;
 
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
-public class TabSwitcher extends View {
+public class TabSwitcher extends LinearLayout {
+    final String LOGTAG = "TabSwitcher";
+    final ListView mList;
+    final Context mContext;
+    final Animation mAnimationFadeIn;
+    final Animation mAnimationFadeOut;
 
     public TabSwitcher(Context context, AttributeSet attrs) {
         super(context, attrs);
-        // TODO Auto-generated constructor stub
+        LayoutInflater.from(context).inflate(R.layout.switcher, this);
+        mContext = context;
+        mList = (ListView) findViewById(R.id.list);
+
+        final ArrayList<Tab> tabs = new ArrayList<Tab>();
+        tabs.add(new Tab());
+        tabs.add(new Tab());
+        tabs.add(new Tab());
+        tabs.add(new Tab()) ;
+        mList.setAdapter(new TabListAdapter(tabs));
+        setOnDragListener(new OnDragListener() {
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+                if (event.getAction() == DragEvent.ACTION_DROP) {
+                    hide();
+                }
+                return true;
+            }
+        });
+        mAnimationFadeOut = AnimationUtils.loadAnimation(mContext, R.anim.fade_out);
+        mAnimationFadeOut.setAnimationListener(new Animation.AnimationListener() {
+            @Override public void onAnimationStart(Animation animation) {}
+            @Override public void onAnimationRepeat(Animation animation) {}
+            @Override public void onAnimationEnd(Animation animation) {
+               setVisibility(View.GONE);
+            }
+        });
+
+        mAnimationFadeIn = AnimationUtils.loadAnimation(mContext, R.anim.fade_in);
+    }
+
+    public void show() {
+        setVisibility(View.VISIBLE);
+        startAnimation(mAnimationFadeIn);
+        mList.requestFocus();
+    }
+
+    public void hide() {
+        startAnimation(mAnimationFadeOut);
+    }
+
+    class TabListAdapter extends ArrayAdapter {
+        final ArrayList<Tab> mTabs;
+
+        public TabListAdapter(ArrayList<Tab> tabs) {
+            super(mContext, R.layout.switcher_list_item, tabs);
+            mTabs = tabs;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+           final Tab tab = mTabs.get(position);
+           if (convertView == null) {
+               convertView =  LayoutInflater.from(getContext()).inflate(R.layout.switcher_list_item, null);
+               convertView.setOnDragListener(new TabItemTouchListener());
+           }
+           return convertView;
+       }
+    }
+
+    class TabItemTouchListener implements OnDragListener {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            //Log.w(LOGTAG, event.getAction());
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    // do nothing
+                    break;
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    v.setAlpha(1f);
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    v.setAlpha(0.5f);
+                    break;
+                case DragEvent.ACTION_DROP:
+                    hide();
+                    break;
+                case DragEvent.ACTION_DRAG_ENDED:
+                  // do nothing
+               default: break;
+            }
+            return true;
+        }
     }
 }
