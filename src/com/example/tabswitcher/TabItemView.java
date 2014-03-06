c5e876de3dafc6ae9006bfaf004531c0a6755ebd
Sola Ogunsakin <oogunsakin@mozilla.com>
2014-03-05 22:36:57 -0800

    restrict item view bounds to wrap content

---
+++ src/com/example/tabswitcher/TabItemView.java
@@ -0,0 +1,14 @@
+package com.example.tabswitcher;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class TabItemView extends View {
+
+    public TabItemView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        // TODO Auto-generated constructor stub
+    }
+
+}
